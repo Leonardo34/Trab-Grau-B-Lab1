232103ff8c1914715986b95652d8538fe058de77
Leonardo34 <leo_broch@hotmail.com>
2016-11-01 16:36:24 -0200

    Iniciado método abreConta e importada Classe Teclado
    Iniciado o método abreConta da Classe Agencia e importada a classe
    Teclado para o projeto

--- Agencia.java
+++ Agencia.java
@@ -1,10 +1,5 @@
+/** Alunos : Leonardo Broch de Morais (leo_broch@hotmail.com) e Lucas Heim        Trabalho B    Lab 1    Prof. Aníbal   2016/2 */ 
 
-/**
- * Escreva a descrição da classe Agencia aqui.
- * 
- * @author (seu nome) 
- * @version (número de versão ou data)
- */
 public class Agencia
 {
     private Poupanca[] contas;
@@ -12,5 +7,32 @@ public class Agencia
     
     public Agencia (int maxContas) {
         this.contas = new Poupanca[maxContas];
+    }
+    
+    public int abreConta () {
+        // Confere se há espaço livre no array para inserir uma nova conta
+        int indexNovaConta = -1;
+        for (int i =0; i < contas.length; i++){
+            if (contas[i] == null) {
+                indexNovaConta = i;
+                break;
+            }
+        }
+        // Se não Há espaço livre no array retorna Erro
+        if (indexNovaConta < 0) {
+            return -1;
+        }
+        // Se há espaço livre continua o processo de instanciar uma nova conta
+        Teclado t = new Teclado();
+        int opcao = t.leInt("1 - Poupanca Simples \n2 - Poupanca Saude");
+        
+        if (opcao == 1) {
+            // Instanciar poupança normal
+        } 
+        else {
+            // Instanciar poupança saude
+        }
+        //Debug
+        return 0;
     }
 }
