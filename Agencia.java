86a1251c1997071f04ec1c5bd304e4f85d9d29fe
Leonardo34 <leo_broch@hotmail.com>
2016-11-01 16:01:05 -0200

    Iniciada Classe Agencia
    Iniciada a Classe Agencia, criado atributos e construtor

---
+++ Agencia.java
@@ -0,0 +1,16 @@
+
+/**
+ * Escreva a descrição da classe Agencia aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class Agencia
+{
+    private Poupanca[] contas;
+    private int totalContas;
+    
+    public Agencia (int maxContas) {
+        this.contas = new Poupanca[maxContas];
+    }
+}
