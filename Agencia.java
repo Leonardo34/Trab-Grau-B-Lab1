de2ba2078d358808c3901029e17ef913a71bb4db
Leonardo34 <leo_broch@hotmail.com>
2016-11-02 11:00:09 -0200

    Iniciado método menuDeTransacoes da Classe Agencia
    Iniciada a implementação do método menuDeTransacoes na Classe Agencia,
    concluidos menu e a opcao para abrir novas contas

--- Agencia.java
+++ Agencia.java
@@ -16,7 +16,7 @@ public class Agencia
      * @return numero da conta instanciada ou codigo de erro (array lotado)
      */
     
-    public int abreConta () {
+    private int abreConta () {
         // Confere se há espaço livre no array para inserir uma nova conta
         int indexNovaConta = -1;
         for (int i =0; i < contas.length; i++){
@@ -43,7 +43,7 @@ public class Agencia
         else {
             // Instanciar poupança saude
             // DEBUG
-            return 0;
+            return -1;
         }       
     }
     
@@ -55,10 +55,10 @@ public class Agencia
      * 
      * @param Numero da conta a ser procurada (numeroConta) 
      * @return indice da conta procurada no array (contas), ou caso não exista
-     * codigo de erro
+     * retorna um codigo de erro
      */
     
-    public int buscaConta (int numeroConta) {
+    private int buscaConta (int numeroConta) {
         for (int i = 0; i < contas.length; i++) {
             if (contas[i].getNumero() == numeroConta) {
                 return i;
@@ -66,4 +66,66 @@ public class Agencia
         }
         return -1;
     }
+    
+    /** 
+     * Método que exibe e opera o sistema da agência
+     * 
+     * @param void 
+     * @return void
+     */
+    
+    public void menuDeTransacoes () {
+        Teclado t = new Teclado();
+        int opcao;
+        
+        do {
+            System.out.println("1- Abre Conta");
+            System.out.println("2- Deposita");
+            System.out.println("3- Retira");
+            System.out.println("4- Retira para Saude");
+            System.out.println("5- Amortiza Financiamento");
+            System.out.println("6- Emite extrato da conta");
+            System.out.println("7- Credita rendimentos");
+            System.out.println("8- Insere um dependente");
+            System.out.println("9- Retira um dependente");
+            System.out.println("10- Encerra");
+            opcao = t.leInt("Digite a opcao escolhida: ");
+            
+            if (opcao == 1) {
+                //Abre uma nova conta na agencia
+                int protocolo = abreConta();
+                if (protocolo < 0) {
+                    System.out.println("Não pode abrir novas contas nessa Agencia");
+                }
+                else {
+                    System.out.println("Conta aberta de numero: " + protocolo);
+                }
+            }
+            else if (opcao == 2) {
+                //Faz deposito
+            }
+            else if (opcao == 3) {
+                //Retira
+            }
+            else if (opcao == 4) {
+                //Retira para a saude
+            }
+            else if (opcao ==5) {
+                //Amortiza para Financiamento
+            }
+            else if (opcao == 6) {
+                //Emite extrato
+            }
+            else if (opcao == 7) {
+                //Credita Rendimentos
+            }
+            else if (opcao == 8) {
+                //Insere um dependente
+            }
+            else if (opcao == 9) {
+                //Retira um dependente
+            }
+        } while (opcao != 10);
+    }
+    
 }
